3159de929c6707b2c222daf29d1ccff13e78915a
Seb <sebastien.poret@hotmail.com>
2023-05-24 17:31:51 +0200

    first commit

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import logo from "../../assets/image/LOGO.png"
+
+export default function Header() {
+  return (
+    <header>
+        <img src={logo} alt="logo de kasa" />
+        <nav>
+            <ul>
+                <li>
+
+                </li>
+                <li>
+
+                </li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
