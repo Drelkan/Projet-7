87ec24ca1e7bcb8ff6b9b570e8ce294718ae1993
Seb <sebastien.poret@hotmail.com>
2023-07-18 21:51:29 +0200

    mise à jours format tablette

--- src/components/reusable/Banner.jsx
+++ src/components/reusable/Banner.jsx
@@ -3,6 +3,7 @@ import React from "react";
 export default function Banner({ title, src }) {
   return (
     <div className="banner">
+      <div className="image-overlay"></div>
       {!title ? "" : <h1 className="banner-title">{title}</h1>}
       <picture>
         <source media="(max-width: 765px)" srcSet={src} />
