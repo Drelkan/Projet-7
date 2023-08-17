f1cb8ecd795d6dc27c5d6bf5306009e4c2f07310
Seb <sebastien.poret@hotmail.com>
2023-08-17 16:20:26 +0200

    Ajout pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,9 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   const [logements, setLogements] = useState([]);
-
   const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const logementsPerPage = 5;
 
   useEffect(() => {
     fetch("./datas/logements.json")
@@ -24,6 +25,10 @@ export default function Home() {
       .catch((error) => console.log(error));
   }, []);
 
+  const indexOfLastLogement = currentPage * logementsPerPage;
+  const indexOfFirstLogement = indexOfLastLogement - logementsPerPage;
+  const currentLogements = logements.slice(indexOfFirstLogement, indexOfLastLogement);
+
   return (
     <div>
       <div className={`custom-loader ${loading ? "" : "hidden"}`}></div>
@@ -32,7 +37,7 @@ export default function Home() {
           <Banner title="Chez vous, partout et ailleurs" src={homeBanner} />
         </div>
         <div className="home-card">
-          {logements.map((logement) => (
+          {currentLogements.map((logement) => (
             <div className="home" key={logement.id}>
               <Link className="logement" to={`/Logement/${logement.id}`}>
                 <img src={logement.cover} alt={logement.alt} />
@@ -43,7 +48,49 @@ export default function Home() {
             </div>
           ))}
         </div>
+        <Pagination
+          logementsPerPage={logementsPerPage}
+          totalLogements={logements.length}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
       </div>
+    </div>
+  );
+}
+
+function Pagination({ logementsPerPage, totalLogements, currentPage, setCurrentPage }) {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalLogements / logementsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <div className="pagination">
+      <button
+        onClick={() => setCurrentPage(currentPage - 1)}
+        disabled={currentPage === 1}
+        className="pagination-button"
+      >
+        &lt; 
+      </button>
+      {pageNumbers.map((number) => (
+        <button
+          key={number}
+          onClick={() => setCurrentPage(number)}
+          className={currentPage === number ? "active" : ""}
+        >
+          {number}
+        </button>
+      ))}
+      <button
+        onClick={() => setCurrentPage(currentPage + 1)}
+        disabled={currentPage === pageNumbers.length}
+        className="pagination-button"
+      >
+        &gt;
+      </button>
     </div>
   );
 }
